e1ae0f74f1c6dbac5cf6620a3e7526ff4739e8c5
irina <ichacon@fi.uba.ar>
2020-12-15 10:17:35 -0300

    creacion del primer mensaje de Tablero

--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -1,6 +1,15 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
+
 public class Tablero {
 
+    private ArrayList <BloqueMovible> secuenciaAEjecutar;
+    private Personaje unPersonaje;
 
+    public void agregarBloque(BloqueMovible unBloque ){
+
+        this.secuenciaAEjecutar.add(unBloque);
+
+    }
 }
