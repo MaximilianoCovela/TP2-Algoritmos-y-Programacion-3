fa1b75baacbdc0bc92fe243974352345e7faceab
Gabriel-Re <gre@fi.uba.ar>
2020-12-15 18:01:36 -0300

    Agrego tests de agregar bloque y lanzar excepcion bloque no encontrado

--- src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TableroTest.java
@@ -1,30 +1,56 @@
 package edu.fiuba.algo3.modelo;
 
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.atMost;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+//import static org.mockito.Mockito.atLeast;
+//import static org.mockito.Mockito.atLeastOnce;
+//import static org.mockito.Mockito.atMost;
+//import static org.mockito.Mockito.inOrder;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.never;
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InOrder;
+import org.junit.rules.ExpectedException;
+//import org.mockito.InOrder;
 
 public class TableroTest {
-   /*
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
     public void testAgregarBloqueDerecha(){
         BloqueDerecha bloqueDerecha = new BloqueDerecha();
         Tablero tablero = new Tablero();
         tablero.agregarBloque(bloqueDerecha);
 
-        //assertEquals(1,)
+
+
+        //assert(tablero.quitarBloque(bloqueDerecha));
+
     }
 
+    @Test
+    public void testAgregoBloqueDerechaYIntentoQuitarBloqueIzquierda(){
+        BloqueMovible bloqueIzquierda = new BloqueIzquierda();
+        BloqueMovible bloqueDerecha = new BloqueDerecha();
 
+        Tablero tablero = new Tablero();
+
+        tablero.agregarBloque(bloqueDerecha);
+
+        expectedException.expect(BloqueNoEncontradoException.class);
+
+        tablero.quitarBloque(bloqueIzquierda);
+
+       //throw new BloqueNoEncontradoException();
+        //assert(tablero.quitarBloque(bloqueDerecha));
+
+    }
+
+/*
     @Test
     public void quitarBloqueEnTableroVacioLanzaExcepcion() {
 
