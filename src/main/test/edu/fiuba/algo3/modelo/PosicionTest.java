0faea1e0bb897ab4adef6a5b120c0c01b37b3a36
Gabriel-Re <gre@fi.uba.ar>
2020-12-16 15:05:35 -0300

    Implementamos los test de Posicion

---
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -0,0 +1,34 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PosicionTest{
+
+    @Test
+    public void testCreaPosicionYChequeoQueSeCreeEnLaPosicionInicialEsperada(){
+        Posicion posicion = new Posicion();
+
+        assertEquals(5, (posicion.getValorHorizontal()));
+
+        assertEquals(5, (posicion.getValorVertical()));
+    }
+
+    @Test
+    public void testModificaLaPosicionYSeActualizaCorrectamente(){
+        Posicion posicion = new Posicion();
+
+        posicion.actualizarPosicion(1,0);
+
+        assertEquals(6, (posicion.getValorHorizontal()));
+
+        assertEquals(5, (posicion.getValorVertical()));
+
+        posicion.actualizarPosicion(0,1);
+
+        assertEquals(6, (posicion.getValorHorizontal()));
+
+        assertEquals(6, (posicion.getValorVertical()));
+    }
+
+}
