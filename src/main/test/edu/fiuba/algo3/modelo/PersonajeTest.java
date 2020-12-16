1393c7fa9514988e78a769692bdf2eb7a035402e
Gabriel-Re <gre@fi.uba.ar>
2020-12-16 17:56:53 -0300

    Agregando tests de personaje

--- src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
@@ -1,16 +1,59 @@
 package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InOrder;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonajeTest {
     @Test
-    public void messageGreeting() {
-        Message message = new Message("Hello world!");
+    public void testMuevePersonajeHaciaAbajo(){
+        Personaje personaje = new Personaje();
+        Movimiento movimiento = new MovimientoAbajo();
 
-        assertEquals("Hello world!", message.greet());
+        personaje.mover(movimiento);
+
+        assertEquals(5, (personaje.getPosicionActual().getValorHorizontal()));
+
+        assertEquals(4, (personaje.getPosicionActual().getValorVertical()));
+
+    }
+
+    @Test
+    public void testMuevePersonajeHaciaArriba(){
+        Personaje personaje = new Personaje();
+        Movimiento movimiento = new MovimientoArriba();
+
+        personaje.mover(movimiento);
+
+        assertEquals(5, (personaje.getPosicionActual().getValorHorizontal()));
+
+        assertEquals(6, (personaje.getPosicionActual().getValorVertical()));
+
+    }
+
+    @Test
+    public void testMuevePersonajeHaciaLaDerecha(){
+        Personaje personaje = new Personaje();
+        Movimiento movimiento = new MovimientoDerecha();
+
+        personaje.mover(movimiento);
+
+        assertEquals(6, (personaje.getPosicionActual().getValorHorizontal()));
+
+        assertEquals(5, (personaje.getPosicionActual().getValorVertical()));
+
+    }
+
+    @Test
+    public void testMuevePersonajeHaciaLaIzquierda(){
+        Personaje personaje = new Personaje();
+        Movimiento movimiento = new MovimientoIzquierda();
+
+        personaje.mover(movimiento);
+
+        assertEquals(4, (personaje.getPosicionActual().getValorHorizontal()));
+
+        assertEquals(5, (personaje.getPosicionActual().getValorVertical()));
+
     }
 }
 
