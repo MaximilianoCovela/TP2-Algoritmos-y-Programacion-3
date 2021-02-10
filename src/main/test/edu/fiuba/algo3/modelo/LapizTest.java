970bf815df776953efbd546eee6485805bfd4859
MaximilianoCovela <maximiliano.covela@outlook.com.ar>
2021-02-10 18:20:34 -0300

    Se agregaron tests que verifican el EstadoLapiz en Figura dentro de LapizTest

--- src/main/test/edu/fiuba/algo3/modelo/LapizTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/LapizTest.java
@@ -1,6 +1,10 @@
 package edu.fiuba.algo3.modelo;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
@@ -23,7 +27,30 @@ public class LapizTest {
         assertTrue(lapiz.esPosibleDibujarCamino());
     }
 
+    @Test
+    public void seVerificaQueLaFiguraSeGuardeConEstadoLapizAbajo() {
+        Lapiz unLapiz = new Lapiz();
+        Posicion antiguaPosicion = new Posicion();
+        Posicion nuevaPosicion = new Posicion();
+        nuevaPosicion.setPosicion(1,0);
+        EstadoLapiz unEstadoLapizAbajo = new LapizAbajo();
+        unLapiz.modificarEstadoLapiz(unEstadoLapizAbajo);
+        unLapiz.dibujarCamino(antiguaPosicion, nuevaPosicion);
+        ArrayList<Figura> arregloSectorDibujo = unLapiz.mostrarSectorDibujo().mostrarFiguras();
+        Figura figura1 = arregloSectorDibujo.get(0);
+        Assertions.assertTrue(figura1.seHaDibujado());
+    }
 
-
+    @Test
+    public void seVerificaQueLaFiguraSeGuardeConEstadoLapizArriba() {
+        Lapiz unLapiz = new Lapiz();
+        Posicion antiguaPosicion = new Posicion();
+        Posicion nuevaPosicion = new Posicion();
+        nuevaPosicion.setPosicion(1,0);
+        unLapiz.dibujarCamino(antiguaPosicion, nuevaPosicion);
+        ArrayList<Figura> arregloSectorDibujo = unLapiz.mostrarSectorDibujo().mostrarFiguras();
+        Figura figura1 = arregloSectorDibujo.get(0);
+        Assertions.assertFalse(figura1.seHaDibujado());
+    }
 }
 
