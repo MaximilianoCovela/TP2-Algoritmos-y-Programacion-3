b013e76c4ce65ab884ed46a571fad8f8adb855f9
irina <ichacon@fi.uba.ar>
2021-02-11 16:36:09 -0300

    Se agregó una prueba en PersonajeTest

--- src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PersonajeTest.java
@@ -72,13 +72,18 @@ public class PersonajeTest {
         Personaje unPersonaje = new Personaje();
         Posicion unaPosicion = new Posicion();
         unaPosicion.setPosicion(1,0);
+
         unPersonaje.actualizarPosicion(unaPosicion);
+
         EstadoLapiz unEstadoLapizAbajo = new LapizAbajo();
         unPersonaje.getLapiz().modificarEstadoLapiz(unEstadoLapizAbajo);
+
         Posicion nuevaPosicion = new Posicion();
         nuevaPosicion.setPosicion(0, 1);
+
         unPersonaje.actualizarPosicion(nuevaPosicion);
         ArrayList<Figura> arregloSectorDibujo = unPersonaje.getLapiz().mostrarSectorDibujo().mostrarFiguras();
+
         Figura figura1 = arregloSectorDibujo.get(0);
         assertFalse(figura1.seHaDibujado());
         Figura figura2 = arregloSectorDibujo.get(1);
@@ -106,13 +111,37 @@ public class PersonajeTest {
         assertEquals(5, (unPersonaje.getPosicionActual().getValorHorizontal()));
         assertEquals(8, (unPersonaje.getPosicionActual().getValorVertical()));
 
+    }
+
+    @Test
+    public void testSeMuevePersonajeYSeBajaLapizYSeChequeaQueCambieElEstadoDelLapizCorrectamente(){
+        MovimientoRepetir unMovimientoRepetir = new MovimientoRepetir(2);
+        Personaje unPersonaje = new Personaje();
+        Movimiento movimientoIzquierda = new MovimientoIzquierda();
+        Movimiento movimientoDerecha = new MovimientoDerecha();
+        Movimiento movimientoArriba = new MovimientoArriba();
+        Movimiento movimientoLapizAbajo = new MovimientoLapizAbajo();
+        Movimiento otroMovimientoArriba = new MovimientoArriba();
+
+        unMovimientoRepetir.agregarMovimiento(movimientoIzquierda);
+        unMovimientoRepetir.agregarMovimiento(movimientoDerecha);
+        unMovimientoRepetir.agregarMovimiento(movimientoArriba);
+
+        unMovimientoRepetir.aplicarMovimiento(unPersonaje);
+        movimientoLapizAbajo.aplicarMovimiento(unPersonaje);
+        otroMovimientoArriba.aplicarMovimiento(unPersonaje);
+
         ArrayList<Figura> arregloSectorDibujo = unPersonaje.getLapiz().mostrarSectorDibujo().mostrarFiguras();
         Figura figura1 = arregloSectorDibujo.get(0);
         assertFalse(figura1.seHaDibujado());
 
-        Figura figura2 = arregloSectorDibujo.get((arregloSectorDibujo.size())-1);
-        assertTrue(figura2.seHaDibujado());
+        Figura figura2 = arregloSectorDibujo.get(1);
+        assertFalse(figura2.seHaDibujado());
+
+        Figura ultimaFigura = arregloSectorDibujo.get((arregloSectorDibujo.size())-1);
+        assertTrue(ultimaFigura.seHaDibujado());
     }
+
 }
 
 
