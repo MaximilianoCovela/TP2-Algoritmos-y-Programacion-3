9b037805e80b27852ddb8d838029fc43ec3ee635
Gabriel-Re <gre@fi.uba.ar>
2021-02-16 00:42:07 -0300

    Se agregan botones a los paneles

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -13,8 +13,14 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import javafx.scene.layout.GridPane;
 import java.io.FileInputStream;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
 
 /**
  * JavaFX App
@@ -32,8 +38,8 @@ public class App extends Application {
         
         stage.setTitle("AlgoBlocks");
 
-        Tablero tablero = new Tablero();
-        this.tablero = tablero;
+        //Tablero tablero = new Tablero();
+        //this.tablero = tablero;
 
         Pane panelTablero = new Pane();
 
@@ -53,6 +59,51 @@ public class App extends Application {
         ImageView imagenVista = new ImageView(new Image("https://i.imgur.com/r0jDjwZ.png"));
         panelSectorDibujo.getChildren().add(imagenVista);
 
+        // VBox
+        VBox vb = new VBox();
+        vb.setPadding(new Insets(10, 50, 50, 50));
+        vb.setSpacing(10);
+
+        Label lbl = new Label("Bloques");
+        lbl.setFont(Font.font("Amble CN", FontWeight.BOLD, 24));
+        vb.getChildren().add(lbl);
+
+        // Buttons
+        Button btn1 = new Button();
+        btn1.setText("Arriba");
+        vb.getChildren().add(btn1);
+
+        Button btn2 = new Button();
+        btn2.setText("Abajo");
+        vb.getChildren().add(btn2);
+
+        Button btn3 = new Button();
+        btn3.setText("Derecha");
+        vb.getChildren().add(btn3);
+
+        Button btn4 = new Button();
+        btn4.setText("Izquierda");
+        vb.getChildren().add(btn4);
+
+        Button btn5 = new Button();
+        btn5.setText("Repetir2");
+        vb.getChildren().add(btn5);
+
+        Button btn6 = new Button();
+        btn6.setText("Repetir3");
+        vb.getChildren().add(btn6);
+
+        Button btn7 = new Button();
+        btn7.setText("Invertir");
+        vb.getChildren().add(btn7);
+
+        Button btn8 = new Button();
+        btn8.setText("Personalizado");
+        vb.getChildren().add(btn8);
+
+        vb.setPrefHeight(438.0);
+        vb.setPrefWidth(172);
+
         ScrollPane panelBloquesDisponibles = new ScrollPane();
 
         panelBloquesDisponibles.setLayoutX(412.0);
@@ -62,6 +113,8 @@ public class App extends Application {
         panelBloquesDisponibles.setMaxSize(172, 438);
 
         panelBloquesDisponibles.setContent(imagenVistaScrollPanel1);
+        panelBloquesDisponibles.setContent(vb);
+
         /*
         Button botonMovimientoArriba = new Button();
         Button botonMovimientoAbajo = new Button();
@@ -107,15 +160,18 @@ public class App extends Application {
         panelBloquesSeleccionados.setLayoutX(618.0);
         panelBloquesSeleccionados.setLayoutY(25.0);
         //cambiar la imagen a una que sea el fondo del scrollpanel
-        String imagePath = "https://p4.wallpaperbetter.com/wallpaper/933/827/743/pokemon-pixel-art-wallpaper-preview.jpg";
-        ImageView imageView = new ImageView(new Image(imagePath));
+        //String imagePath = "https://p4.wallpaperbetter.com/wallpaper/933/827/743/pokemon-pixel-art-wallpaper-preview.jpg";
+        //ImageView imageView = new ImageView(new Image(imagePath));
         panelBloquesSeleccionados.setMaxSize(172, 438);
-
-        panelBloquesSeleccionados.setContent(imageView);
+        VBox vb2 = new VBox();
+        vb2.setPrefHeight(438.0);
+        vb2.setPrefWidth(172);
+        panelBloquesSeleccionados.setContent(vb2);
 
         panelTablero.getChildren().add(panelSectorDibujo);
         panelTablero.getChildren().add(panelBloquesDisponibles);
         panelTablero.getChildren().add(panelBloquesSeleccionados);
+
 
         Scene scene = new Scene(panelTablero);
 
